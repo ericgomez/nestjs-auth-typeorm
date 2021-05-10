b2843355c4361c77e12871703acc409eb5279059
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-05-10 12:02:29 -0500

    Crear, actualizar y eliminar desde Nest a Postgres

--- src/products/services/products.service.ts
+++ src/products/services/products.service.ts
@@ -15,40 +15,39 @@ export class ProductsService {
     return this.productRepo.find(); // 👈 use repo
   }
 
-  findOne(id: number) {
-    const product = this.productRepo.findOne(id); // 👈 use repo
+  // Cambiamos la funcion de manera asincrona para poder validar cuando un producto no fue encontrado
+  async findOne(id: number) {
+    const product = await this.productRepo.findOne(id); // 👈 use repo
     if (!product) {
       throw new NotFoundException(`Product #${id} not found`);
     }
     return product;
   }
 
-  // create(data: CreateProductDto) {
-  //   this.counterId = this.counterId + 1;
-  //   const newProduct = {
-  //     id: this.counterId,
-  //     ...data,
-  //   };
-  //   this.products.push(newProduct);
-  //   return newProduct;
-  // }
+  create(data: CreateProductDto) {
+    // Forma 1 - Declarando un por uno
+    // const newProduct = new Product();
+    // newProduct.name = data.image;
+    // newProduct.description = data.description;
+    // newProduct.price = data.price;
+    // newProduct.stock = data.stock;
+    // newProduct.image = data.image;
 
-  // update(id: number, changes: UpdateProductDto) {
-  //   const product = this.findOne(id);
-  //   const index = this.products.findIndex((item) => item.id === id);
-  //   this.products[index] = {
-  //     ...product,
-  //     ...changes,
-  //   };
-  //   return this.products[index];
-  // }
+    // Forma 1 - Creando un instancia
+    // De esta manera podemos instanciar de manera mas facil todos los elementos de un producto
+    const newProduct = this.productRepo.create(data);
+    return this.productRepo.save(newProduct); // Guradamos en la base de datos
+  }
 
-  // remove(id: number) {
-  //   const index = this.products.findIndex((item) => item.id === id);
-  //   if (index === -1) {
-  //     throw new NotFoundException(`Product #${id} not found`);
-  //   }
-  //   this.products.splice(index, 1);
-  //   return true;
-  // }
+  async update(id: number, changes: UpdateProductDto) {
+    const product = await this.productRepo.findOne(id); // Obtenemos el producto a actualizar
+    // Con merge realizamos los cambio recibiendo: (Producto a actualizar, y los cambios que se tiene que aplicar)
+    this.productRepo.merge(product, changes);
+
+    return this.productRepo.save(product); // Guradamos en la base de datos
+  }
+
+  remove(id: number) {
+    return this.productRepo.delete(id);
+  }
 }
